afb26bb6c1991edb869aff1c70eb6a4e6b602029
lnlindao <lnlindao@hotmail.com>
2017-07-04 14:03:36 -0500

    Tabs layout adicional

---
+++ TabLayout/app/src/main/java/com/example/pc/tablayout/Tab1.java
@@ -0,0 +1,21 @@
+package com.example.pc.tablayout;
+
+/**
+ * Created by PC on 04/07/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1 extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+}
